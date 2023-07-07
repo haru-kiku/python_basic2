922d71c0a82cbd1a65fdb1fb853bd947f84a95b3
haru-kiku <haru11250321@gmail.com>
2023-07-07 21:38:10 +0900

    1課題b-2

--- kuku_2.py
+++ kuku_2.py
@@ -1,6 +1,10 @@
-def range()
+row = int(input("行数を入力してください"))
+col = int(input("列数を入力してください"))
 
-for i in range(1, 5):
-    for j in range(1, 7):
+
+
+
+for i in range(1, row+1):
+    for j in range(1, col+1):
         print(i * j, end="")
     print()
